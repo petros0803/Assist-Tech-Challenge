b4397dcdd00e13570818471293704df88e987152
Saxdel <45599037+Saxdel@users.noreply.github.com>
2021-04-07 22:08:47 +0300

    Sending email first try

---
+++ HotelAutomation.Core/Services/MailService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using HotelAutomation.Application.Common.Interfaces.Repositories;
+using HotelAutomation.Application.Common.Models.MailModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace HotelAutomation.Application.Services
+{
+    public class MailService
+    {
+        private readonly IMailRepository mailRepository;
+        private readonly IReservationRepository reservationRepository;
+        private readonly IRoomRepository roomRepository;
+        private readonly IUserRepository userRepository;
+        private readonly IConfiguration configuration;
+        private readonly IMapper mapper;
+
+        public MailService(IReservationRepository reservationRepository, IRoomRepository roomRepository, IUserRepository userRepository)
+        {
+            this.reservationRepository = reservationRepository;
+            this.roomRepository = roomRepository;
+            this.userRepository = userRepository;
+            //this.configuration = configuration;
+        }
+        public void SendEmail(MailRequest mail)
+        {
+            MailAddress to = new MailAddress(mail.ToEmail);
+            MailAddress from = new MailAddress("");
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = mail.Subject;
+            message.Body = mail.Body;
+            SmtpClient client = new SmtpClient("smtp.server.address", 2525)
+            {
+              
+                Credentials = new NetworkCredential("smtp_username", "smtp_password"),
+                EnableSsl = true
+            };
+            
+             client.Send(message);
+            
+        }
+    }
+}   
+
---
+++ HotelAutomation.Infrastructure/Database/Repositories/MailRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelAutomation.Infrastructure.Database.Repositories
+{
+    public class MailRepository
+    {
+    }
+}
